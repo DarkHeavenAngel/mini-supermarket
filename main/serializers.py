3373fb692bedc97e7e9f6ba7f150bf232dfef074
OlgaMFox <olya.marushchenko@gmail.com>
2026-05-19 21:16:42 +0300

    Fixed problems

--- main/serializers.py
+++ main/serializers.py
@@ -13,17 +13,23 @@ class ProductSerializer(serializers.ModelSerializer):
         fields = ['id_product', 'category_number', 'product_name', 'characteristics', 'manufacturer']
 
 class StoreProductSerializer(serializers.ModelSerializer):
-    upc_prom = serializers.PrimaryKeyRelatedField(queryset = None, required = False, allow_null = True)
+    upc_prom = serializers.PrimaryKeyRelatedField(queryset = StoreProduct.objects.all(), required = False, allow_null = True)
     class Meta:
         model = StoreProduct
         fields = ['upc', 'upc_prom', 'id_product', 'selling_price', 'products_number', 'promotional_product']
 
 class EmployeeSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
     class Meta:
         model = Employee
         fields = ['id_employee', 'empl_surname', 'empl_name', 'empl_patronymic',
                   'empl_role', 'salary', 'date_of_birth', 'date_of_start',
-                  'phone_number', 'city', 'street', 'zip_code']
+                  'phone_number', 'city', 'street', 'zip_code', 'password']
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = Employee.objects.create_user(password=password, **validated_data)
+        return user
 
 class CustomerCardSerializer(serializers.ModelSerializer):
     class Meta:
@@ -35,8 +41,12 @@ class CheckSerializer(serializers.ModelSerializer):
     class Meta:
         model = Check
         fields = ['check_number', 'id_employee', 'card_number', 'print_date', 'sum_total', 'vat']
+        read_only_fields = ['print_date', 'sum_total', 'vat']
 
 class SaleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Sale
         fields = ['upc', 'check_number', 'product_number', 'selling_price']
+        read_only_fields = ['selling_price']
+
+
