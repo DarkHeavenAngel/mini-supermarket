cf92ac326f884f86a5211819ca7948143ecabfd3
plusik <o.mykhailyk@ukma.edu.ua>
2026-05-27 17:14:39 +0300

    fixed cors

--- mini_supermarket/settings.py
+++ mini_supermarket/settings.py
@@ -28,8 +28,8 @@ DEBUG = True
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:8001",
-    "http://localhost:8001",
+    "http://127.0.0.1:8000",
+    "http://localhost:8000",
 ]
 
 # Application definition
